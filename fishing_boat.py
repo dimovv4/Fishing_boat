710f73f1319d8e433ed6a775cff42c4c7a7e985e
Georgi Dimov <147717567+dimovv4@users.noreply.github.com>
2023-11-14 18:03:40 +0200

    Create fishing_boat.py

---
+++ fishing_boat.py
@@ -0,0 +1,23 @@
+budget = int(input())
+season = input()
+number_of_fishermen = int(input())
+boat_rent = 0
+
+if season == "Spring":
+    boat_rent = 3000
+elif season == "Summer" or season == "Autumn":
+    boat_rent = 4200
+elif season == "Winter":  # else
+    boat_rent = 2600
+if number_of_fishermen <= 6:
+    boat_rent *= 0.9
+elif number_of_fishermen <= 11:  #elif 7 <= number_of_fishermen <= 11:
+    boat_rent *= 0.85
+elif number_of_fishermen >= 12:  #else:
+    boat_rent *= 0.75
+if number_of_fishermen % 2 == 0 and season != "Autumn":
+    boat_rent *= 0.95
+if budget >= boat_rent:
+    print(f"Yes! You have {} leva left.")
+else:
+    print(f"Not enough money! You need {difference:.2f} leva.")
